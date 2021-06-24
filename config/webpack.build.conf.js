d16b96867a30b7854c777f3239e16b516538fc97
2Machiavelli <stas.chetyrbok@mail.ru>
2021-06-24 16:37:11 +0300

    STYLE : remove cleanwebpackplugin for using webpack 5 solution

--- config/webpack.build.conf.js
+++ config/webpack.build.conf.js
@@ -1,16 +1,13 @@
 const merge                    = require('webpack-merge')
 const baseWebpackConfig        = require('./webpack.base.conf')
-const { CleanWebpackPlugin }   = require('clean-webpack-plugin')
 
 const buildWebpackConfig = merge(baseWebpackConfig, {
   mode: 'production',
   output: {
-    publicPath: './'
+    publicPath: './',
+    clean: true
   },
   devtool: 'source-map',
-  plugins: [
-    new CleanWebpackPlugin()
-  ]
 })
 
 module.exports = new Promise((resolve, reject) => {
